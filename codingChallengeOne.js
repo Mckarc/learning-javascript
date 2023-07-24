e82f584e40a2a22c83c5b169a7f70a89caf7300b
Mckarc <michaelandre752@gmail.com>
2023-07-24 05:41:59 -0400

    Updated codingChallengeOne.js codingChallengeTwo.js index.html script.js

--- codingChallengeOne.js
+++ codingChallengeOne.js
@@ -1,13 +1,37 @@
-let markMass = 78;
-let markHeight = 1.69;
-let johnMass = 95;
-let johnHeight = 1.88;
+// First challenge
+// let markMass = 78;
+// let markHeight = 1.69;
+// let johnMass = 95;
+// let johnHeight = 1.88;
 
-let markBMI = markMass / markHeight ** 2;
-console.log(markBMI);
+// let markBMI = markMass / markHeight ** 2;
+// let johnBMI = johnMass / (johnHeight * johnHeight);
+// let markHigherBMI = markBMI > johnBMI;
+// console.log(markBMI, johnBMI, markHigherBMI);
 
-let johnBMI = johnMass / (johnHeight * johnHeight);
-console.log(johnBMI);
+// Second challenge
+// if (markBMI > johnBMI) {
+//   console.log(`Marks's BMI (${markBMI}) is higher than John's (${johnBMI})!`);
+// } else {
+//   console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`);
+// }
 
-let markHigherBMI = markBMI > johnBMI;
-console.log(markHigherBMI);
+// Third challenge
+let scoresDolphins;
+let scoresKoalas;
+
+const averageValue = 3;
+const dolphinsDataOne = 96 + 108 + 89;
+const koalasDataOne = 88 + 91 + 110;
+
+scoresDolphins = dolphinsDataOne / averageValue;
+scoresKoalas = koalasDataOne / averageValue;
+console.log(scoresDolphins, scoresKoalas);
+
+if (scoresDolphins > scoresKoalas) {
+  console.log(`Dolphins win the trophy 🏆 with ${scoresDolphins}`);
+} else if (scoresKoalas > scoresDolphins) {
+  console.log(`Koalas win the the trophy 🏆 with ${scoresKoalas}`);
+} else if (scoresDolphins === scoresKoalas) {
+  console.log(`Both win the trophy 🏆`);
+}
--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-alert("Hello");
+let fave = Number(prompt("Please enter your favorite number"));
+// console.log(Number(fave), typeof fave);
 
-console.log("Hello");
-console.log("World");
+if (fave === 25) {
+  console.log("Number is 25");
+} else if (fave === 7) {
+  console.log("Number is 7");
+} else {
+  console.log("Number is not 25 or 7");
+}
+
+if (fave !== 25) console.log("Why not 25");
